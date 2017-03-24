9c6ca13242144f2b831e7c825d8f7d21720b0a91
Joaquin <joaquin@autotel.co>
2017-03-24 12:57:34 +0200

    created voice number in db

--- js/database.js
+++ js/database.js
@@ -1,233 +1,320 @@
+/*
+regex snippets:
+#find all by name component:
+
+#created a new field in db
+	*find _Part(\d\d).*
+	*replace $&,\n\t\t"voicen":$1
+
+
+*/
 var loopsDatabase=[{
     "name": "organ minor",
-    "source": "audio/024_OcanBridg0  _Part04.mp3"
+    "source": "audio/024_OcanBridg0  _Part04.mp3",
+		"voicen":04
 }, {
     "name": "djembe low pat 01",
-    "source": "audio/024_OcanBridg0  _Part05.mp3"
+    "source": "audio/024_OcanBridg0  _Part05.mp3",
+		"voicen":05
 }, {
     "name": "djembe hi pat 02",
-    "source": "audio/024_OcanBridg0  _Part06.mp3"
+    "source": "audio/024_OcanBridg0  _Part06.mp3",
+		"voicen":06
 }, {
     "name": "hey nrm pat 01",
-    "source": "audio/024_OcanBridg0  _Part07.mp3"
+    "source": "audio/024_OcanBridg0  _Part07.mp3",
+		"voicen":07
 }, {
     "name": "kick hi pat 01",
-    "source": "audio/024_OcanBridg0  _Part09.mp3"
+    "source": "audio/024_OcanBridg0  _Part09.mp3",
+		"voicen":09
 }, {
     "name": "clap nrm pat 1",
-    "source": "audio/024_OcanBridg0  _Part11.mp3"
+    "source": "audio/024_OcanBridg0  _Part11.mp3",
+		"voicen":11
 }, {
     "name": "kick low pat 01",
-    "source": "audio/024_OcanBridg0  _Part12.mp3"
+    "source": "audio/024_OcanBridg0  _Part12.mp3",
+		"voicen":12
 }, {
     "name": "ride nrm pat 01",
-    "source": "audio/024_OcanBridg0  _Part14.mp3"
+    "source": "audio/024_OcanBridg0  _Part14.mp3",
+		"voicen":14
 }, {
     "name": "djembe gesture pat 01",
-    "source": "audio/024_OcanBridg0  _Part16.mp3"
+    "source": "audio/024_OcanBridg0  _Part16.mp3",
+		"voicen":16
 }, {
     "name": "bass acid pat 01",
-    "source": "audio/025_OcanChor1   _Part01.mp3"
+    "source": "audio/025_OcanChor1   _Part01.mp3",
+		"voicen":01
 }, {
     "name": "pad nrm pat minor",
-    "source": "audio/025_OcanChor1   _Part02.mp3"
+    "source": "audio/025_OcanChor1   _Part02.mp3",
+		"voicen":02
 }, {
     "name": "organ merry chords",
-    "source": "audio/025_OcanChor1   _Part04.mp3"
+    "source": "audio/025_OcanChor1   _Part04.mp3",
+		"voicen":04
 }, {
     "name": "djembe low pat 02",
-    "source": "audio/025_OcanChor1   _Part05.mp3"
+    "source": "audio/025_OcanChor1   _Part05.mp3",
+		"voicen":05
 }, {
     "name": "djembe hi pat 02",
-    "source": "audio/025_OcanChor1   _Part06.mp3"
+    "source": "audio/025_OcanChor1   _Part06.mp3",
+		"voicen":06
 }, {
     "name": "hey nrm pat 02",
-    "source": "audio/025_OcanChor1   _Part07.mp3"
+    "source": "audio/025_OcanChor1   _Part07.mp3",
+		"voicen":07
 }, {
     "name": "shaker nrm pat 01.mp3",
-    "source": "audio/025_OcanChor1   _Part08.mp3"
+    "source": "audio/025_OcanChor1   _Part08.mp3",
+		"voicen":08
 }, {
     "name": "kick hi pat 02.mp3",
-    "source": "audio/025_OcanChor1   _Part09.mp3"
+    "source": "audio/025_OcanChor1   _Part09.mp3",
+		"voicen":09
 }, {
     "name": "snare merry",
-    "source": "audio/025_OcanChor1   _Part10.mp3"
+    "source": "audio/025_OcanChor1   _Part10.mp3",
+		"voicen":10
 }, {
     "name": "clap nrm",
-    "source": "audio/025_OcanChor1   _Part11.mp3"
+    "source": "audio/025_OcanChor1   _Part11.mp3",
+		"voicen":11
 }, {
     "name": "kick low",
-    "source": "audio/025_OcanChor1   _Part12.mp3"
+    "source": "audio/025_OcanChor1   _Part12.mp3",
+		"voicen":12
 }, {
     "name": "djembe higher",
-    "source": "audio/025_OcanChor1   _Part15.mp3"
+    "source": "audio/025_OcanChor1   _Part15.mp3",
+		"voicen":15
 }, {
     "name": "djembe gesture",
-    "source": "audio/025_OcanChor1   _Part16.mp3"
+    "source": "audio/025_OcanChor1   _Part16.mp3",
+		"voicen":16
 }, {
     "name": "bass acid",
-    "source": "audio/026_OcanBridg1  _Part01.mp3"
+    "source": "audio/026_OcanBridg1  _Part01.mp3",
+		"voicen":01
 }, {
     "name": "pad nrm",
-    "source": "audio/026_OcanBridg1  _Part02.mp3"
+    "source": "audio/026_OcanBridg1  _Part02.mp3",
+		"voicen":02
 }, {
     "name": "organ super",
-    "source": "audio/026_OcanBridg1  _Part03.mp3"
+    "source": "audio/026_OcanBridg1  _Part03.mp3",
+		"voicen":03
 }, {
     "name": "organ merry",
-    "source": "audio/026_OcanBridg1  _Part04.mp3"
+    "source": "audio/026_OcanBridg1  _Part04.mp3",
+		"voicen":04
 }, {
     "name": "hey nrm",
-    "source": "audio/026_OcanBridg1  _Part07.mp3"
+    "source": "audio/026_OcanBridg1  _Part07.mp3",
+		"voicen":07
 }, {
     "name": "shaker nrm",
-    "source": "audio/026_OcanBridg1  _Part08.mp3"
+    "source": "audio/026_OcanBridg1  _Part08.mp3",
+		"voicen":08
 }, {
     "name": "kick hi",
-    "source": "audio/026_OcanBridg1  _Part09.mp3"
+    "source": "audio/026_OcanBridg1  _Part09.mp3",
+		"voicen":09
 }, {
     "name": "snare merry",
-    "source": "audio/026_OcanBridg1  _Part10.mp3"
+    "source": "audio/026_OcanBridg1  _Part10.mp3",
+		"voicen":10
 }, {
     "name": "kick low",
-    "source": "audio/026_OcanBridg1  _Part12.mp3"
+    "source": "audio/026_OcanBridg1  _Part12.mp3",
+		"voicen":12
 }, {
     "name": "bass acid",
-    "source": "audio/027_OcanBridg2  _Part01.mp3"
+    "source": "audio/027_OcanBridg2  _Part01.mp3",
+		"voicen":01
 }, {
     "name": "pad nrm",
-    "source": "audio/027_OcanBridg2  _Part02.mp3"
+    "source": "audio/027_OcanBridg2  _Part02.mp3",
+		"voicen":02
 }, {
     "name": "djembe low pat",
-    "source": "audio/027_OcanBridg2  _Part05.mp3"
+    "source": "audio/027_OcanBridg2  _Part05.mp3",
+		"voicen":05
 }, {
     "name": "djembe hi pat",
-    "source": "audio/027_OcanBridg2  _Part06.mp3"
+    "source": "audio/027_OcanBridg2  _Part06.mp3",
+		"voicen":06
 }, {
     "name": "hey nrm",
-    "source": "audio/027_OcanBridg2  _Part07.mp3"
+    "source": "audio/027_OcanBridg2  _Part07.mp3",
+		"voicen":07
 }, {
     "name": "kick hi",
-    "source": "audio/027_OcanBridg2  _Part09.mp3"
+    "source": "audio/027_OcanBridg2  _Part09.mp3",
+		"voicen":09
 }, {
     "name": "snare merry",
-    "source": "audio/027_OcanBridg2  _Part10.mp3"
+    "source": "audio/027_OcanBridg2  _Part10.mp3",
+		"voicen":10
 }, {
     "name": "clap nrm",
-    "source": "audio/027_OcanBridg2  _Part11.mp3"
+    "source": "audio/027_OcanBridg2  _Part11.mp3",
+		"voicen":11
 }, {
     "name": "kick low",
-    "source": "audio/027_OcanBridg2  _Part12.mp3"
+    "source": "audio/027_OcanBridg2  _Part12.mp3",
+		"voicen":12
 }, {
-    "name": "027_OcanBridg2  _Part13.mp3",
-    "source": "audio/027_OcanBridg2  _Part13.mp3"
+    "name": "hh a.mp3",
+    "source": "audio/027_OcanBridg2  _Part13.mp3",
+		"voicen":13
 }, {
-    "name": "027_OcanBridg2  _Part14.mp3",
-    "source": "audio/027_OcanBridg2  _Part14.mp3"
+    "name": "hh b.mp3",
+    "source": "audio/027_OcanBridg2  _Part14.mp3",
+		"voicen":14
 }, {
     "name": "organ super",
-    "source": "audio/028_OcanChor1Var1_Part03.mp3"
+    "source": "audio/028_OcanChor1Var1_Part03.mp3",
+		"voicen":03
 }, {
     "name": "organ merry",
-    "source": "audio/028_OcanChor1Var1_Part04.mp3"
+    "source": "audio/028_OcanChor1Var1_Part04.mp3",
+		"voicen":04
 }, {
     "name": "djembe low pat",
-    "source": "audio/028_OcanChor1Var1_Part05.mp3"
+    "source": "audio/028_OcanChor1Var1_Part05.mp3",
+		"voicen":05
 }, {
     "name": "djembe hi pat",
-    "source": "audio/028_OcanChor1Var1_Part06.mp3"
+    "source": "audio/028_OcanChor1Var1_Part06.mp3",
+		"voicen":06
 }, {
     "name": "hey nrm",
-    "source": "audio/028_OcanChor1Var1_Part07.mp3"
+    "source": "audio/028_OcanChor1Var1_Part07.mp3",
+		"voicen":07
 }, {
     "name": "shaker nrm",
-    "source": "audio/028_OcanChor1Var1_Part08.mp3"
+    "source": "audio/028_OcanChor1Var1_Part08.mp3",
+		"voicen":08
 }, {
     "name": "kick hi",
-    "source": "audio/028_OcanChor1Var1_Part09.mp3"
+    "source": "audio/028_OcanChor1Var1_Part09.mp3",
+		"voicen":09
 }, {
     "name": "snare merry",
-    "source": "audio/028_OcanChor1Var1_Part10.mp3"
+    "source": "audio/028_OcanChor1Var1_Part10.mp3",
+		"voicen":10
 }, {
     "name": "clap nrm",
-    "source": "audio/028_OcanChor1Var1_Part11.mp3"
+    "source": "audio/028_OcanChor1Var1_Part11.mp3",
+		"voicen":11
 }, {
     "name": "kick low",
-    "source": "audio/028_OcanChor1Var1_Part12.mp3"
+    "source": "audio/028_OcanChor1Var1_Part12.mp3",
+		"voicen":12
 }, {
     "name": "organ super",
-    "source": "audio/029_OcanChor1Var2_Part03.mp3"
+    "source": "audio/029_OcanChor1Var2_Part03.mp3",
+		"voicen":03
 }, {
     "name": "organ merry",
-    "source": "audio/029_OcanChor1Var2_Part04.mp3"
+    "source": "audio/029_OcanChor1Var2_Part04.mp3",
+		"voicen":04
 }, {
     "name": "djembe low pat",
-    "source": "audio/029_OcanChor1Var2_Part05.mp3"
+    "source": "audio/029_OcanChor1Var2_Part05.mp3",
+		"voicen":05
 }, {
     "name": "djembe hi pat",
-    "source": "audio/029_OcanChor1Var2_Part06.mp3"
+    "source": "audio/029_OcanChor1Var2_Part06.mp3",
+		"voicen":06
 }, {
     "name": "hey nrm",
-    "source": "audio/029_OcanChor1Var2_Part07.mp3"
+    "source": "audio/029_OcanChor1Var2_Part07.mp3",
+		"voicen":07
 }, {
     "name": "shaker nrm",
-    "source": "audio/029_OcanChor1Var2_Part08.mp3"
+    "source": "audio/029_OcanChor1Var2_Part08.mp3",
+		"voicen":08
 }, {
     "name": "kick hi",
-    "source": "audio/029_OcanChor1Var2_Part09.mp3"
+    "source": "audio/029_OcanChor1Var2_Part09.mp3",
+		"voicen":09
 }, {
     "name": "snare merry",
-    "source": "audio/029_OcanChor1Var2_Part10.mp3"
+    "source": "audio/029_OcanChor1Var2_Part10.mp3",
+		"voicen":10
 }, {
     "name": "clap nrm",
-    "source": "audio/029_OcanChor1Var2_Part11.mp3"
+    "source": "audio/029_OcanChor1Var2_Part11.mp3",
+		"voicen":11
 }, {
     "name": "kick low",
-    "source": "audio/029_OcanChor1Var2_Part12.mp3"
+    "source": "audio/029_OcanChor1Var2_Part12.mp3",
+		"voicen":12
 }, {
     "name": "djembe gesture",
-    "source": "audio/029_OcanChor1Var2_Part16.mp3"
+    "source": "audio/029_OcanChor1Var2_Part16.mp3",
+		"voicen":16
 }, {
     "name": "bass acid",
-    "source": "audio/030_OcanClos1Var1_Part01.mp3"
+    "source": "audio/030_OcanClos1Var1_Part01.mp3",
+		"voicen":01
 }, {
     "name": "organ super",
-    "source": "audio/030_OcanClos1Var1_Part03.mp3"
+    "source": "audio/030_OcanClos1Var1_Part03.mp3",
+		"voicen":03
 }, {
     "name": "organ merry",
-    "source": "audio/030_OcanClos1Var1_Part04.mp3"
+    "source": "audio/030_OcanClos1Var1_Part04.mp3",
+		"voicen":04
 }, {
     "name": "djembe low pat",
-    "source": "audio/030_OcanClos1Var1_Part05.mp3"
+    "source": "audio/030_OcanClos1Var1_Part05.mp3",
+		"voicen":05
 }, {
     "name": "djembe hi pat",
-    "source": "audio/030_OcanClos1Var1_Part06.mp3"
+    "source": "audio/030_OcanClos1Var1_Part06.mp3",
+		"voicen":06
 }, {
     "name": "hey nrm",
-    "source": "audio/030_OcanClos1Var1_Part07.mp3"
+    "source": "audio/030_OcanClos1Var1_Part07.mp3",
+		"voicen":07
 }, {
     "name": "shaker nrm",
-    "source": "audio/030_OcanClos1Var1_Part08.mp3"
+    "source": "audio/030_OcanClos1Var1_Part08.mp3",
+		"voicen":08
 }, {
     "name": "kick hi",
-    "source": "audio/030_OcanClos1Var1_Part09.mp3"
+    "source": "audio/030_OcanClos1Var1_Part09.mp3",
+		"voicen":09
 }, {
     "name": "snare merry",
-    "source": "audio/030_OcanClos1Var1_Part10.mp3"
+    "source": "audio/030_OcanClos1Var1_Part10.mp3",
+		"voicen":10
 }, {
     "name": "clap nrm",
-    "source": "audio/030_OcanClos1Var1_Part11.mp3"
+    "source": "audio/030_OcanClos1Var1_Part11.mp3",
+		"voicen":11
 }, {
     "name": "kick low",
-    "source": "audio/030_OcanClos1Var1_Part12.mp3"
+    "source": "audio/030_OcanClos1Var1_Part12.mp3",
+		"voicen":12
 }, {
     "name": "djembe higher",
-    "source": "audio/030_OcanClos1Var1_Part15.mp3"
+    "source": "audio/030_OcanClos1Var1_Part15.mp3",
+		"voicen":15
 }, {
     "name": "djembe gesture",
-    "source": "audio/030_OcanClos1Var1_Part16.mp3"
+    "source": "audio/030_OcanClos1Var1_Part16.mp3",
+		"voicen":16
 }, {
     "name": "percussion swing 00",
-    "source": "audio/percBack.mp3"
+    "source": "audio/percBack.mp3",
+		"voicen":0
 }
 ]
